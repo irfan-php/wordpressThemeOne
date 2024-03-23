db02984e41909f1b8843e3d906e664dcb56c9d94
Mohammed Irfan <mohd5.irfan@outlook.com>
2024-03-23 14:28:25 +0530

    First Push

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Main template file
+ * 
+ * @package ThemeOne
+ */
+?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ThemeOne</title>
+ </head>
+ <body>
+    Hello World
+ </body>
+ </html>
